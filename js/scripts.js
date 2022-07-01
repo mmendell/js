d00cad771bad9a2b6beb6dc6ae77c5c8dd75a67e
mmendell <105883493+mmendell@users.noreply.github.com>
2022-07-01 12:37:41 -0400

    added loop
    attempted to add loop for function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,33 @@
+let space = "  "
 let pokemonList = [
-    { 'name': 'bulbasur', 'size': 7, 'type': 'grass' };
-    { 'name': 'charmader', 'size': 0.6, 'type': 'fire' };
-    { 'name': 'kakuna', 'size':  0.6, 'type': 'bug'};
+    {
+        name: "bulbasur",
+        size: 7,
+        type: "grass"
+    },
+    {
+        name: "charmader",
+        size: 0.6,
+        type: "fire"
+    },
+    {
+        name: "kakuna",
+        size: 0.6,
+        type: "bug"
+    },
 ];
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemmonList[i].size > 1) {
+        document.write(pokemonList[i].name + 'what a massiv ething you may appear as')
+        
+    } else if (pokemonList[i].type == 'grass') {
+        document.write(pokemonList[i].name + ' (height' + pokemonList[i].size + ')')
+    } else {
+        document.write(pokemonList[i].name +' (height' + pokemonList[i].size + ')')
+    }
+    
+};
+
+
