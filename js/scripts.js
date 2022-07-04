1a12c5ad5bbd2e1e3d411d6b825fbb264e2e0fed
mmendell <105883493+mmendell@users.noreply.github.com>
2022-07-04 17:11:09 -0400

    added functions
    switched to iife.
    added functions with iife

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,28 @@
-let space = "  "
-let pokemonList = [
-    {
-        name: "bulbasur",
-        size: 7,
-        type: "grass"
-    },
-    {
-        name: "charmader",
-        size: 0.6,
-        type: "fire"
-    },
-    {
-        name: "kakuna",
-        size: 0.6,
-        type: "bug"
-    },
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+            {
+            name: "bulbasur",
+            size: 7,
+            type: "grass"
+        },
+        {
+            name: "charmader",
+            size: 0.6,
+            type: "fire"
+        },
+        {
+            name: "kakuna",
+            size: 0.6,
+            type: "bug"
+        },    
+    ];
+    function getAll() {
+        return pokemonList;
+    }
 
-pokemonList.forEach(pokemonList => {
-    document.write(pokemonList.name + " is " + pokemonList.size + ' tall' + '<br>')
-});
+    function add() {
+        pokemonRepository.add(item);
+    }
+
+  })();
+  
