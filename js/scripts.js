0d66aacdde6a23f763f914c9a9b00ef17d49727c
mmendell <105883493+mmendell@users.noreply.github.com>
2022-07-07 14:37:31 -0400

    modal
    attempted to add modal

--- js/scripts.js
+++ js/scripts.js
@@ -58,6 +58,7 @@ let pokemonRepository = (function () {
             item.imageUrl = details.sprites.front_default;
             item.height = details.height;
             item.types = details.types;
+            item.name = details.name;
         }).catch(function (e) {
             console.error(e);
         });
@@ -66,22 +67,86 @@ let pokemonRepository = (function () {
 
     function showDetails(item) {
         loadDetails(item).then(function () {
-            console.log(item);
+            let modalContainer = document.modalContainer('#modal-container');
+            modalContainer.innerHTML = '';
+
+            let modal = document.createElement('div');
+            modal.classList.add('modal');
+
+            let closeButton = document.createElement('button');
+            closeButton.classList.add('close-modal');
+            closeButton.innerText = 'close';
+            closeButton.addEventListener('click', hideModal);
+
+            let pokeName = document.createElement('h1');
+            pokeName.innerText = item.name;
+
+            let pokeImg = document.createElement('img');
+            pokeImg.src = item.imageUrl;
+
+            let pokeHeight = document.createElement('p');
+            pokeHeight.innerText = item.height;
+
+            let pokeType = document.createElement('p');
+            pokeType.innerText = item.height;
+
+            modal.appendChild(closeButton);
+            modal.appendChild(pokeName);
+            modal.appendChild(pokeImg);
+            modal.appendChild(pokeHeight);
+            modal.appendChild(pokeType);
+            modalContainer.appendChild(modal);
+
+
+            modal.classList.add('is visible');
+
         });
-    };
+
+        let dialogPromiseReject;
+
+        function hideModal() {
+            modalContainer.classList.remove('is-visible');
+
+            if (dialogPromiseReject) {
+                dialogPromiseReject();
+                dialogPromiseReject = null;
+            }
+        }
+
+        document.querySelector('#show-modal').addEventListener('click', () => {
+            showDetails();
+        })
+
+        window.addEventListener('keydown', (e) => {
+            if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+                hideModal();
+            }
+        });
+
+
+        modalContainer.addEventListener('click', (e) => {
+            let target = e.target;
+            if (target === modalContainer) {
+                hideModal();
+            }
+        });
+          
+        ));
 
 
 
-    return {
-        add: add,
-        getAll: getAll,
-        addListItem: addListItem,
-        loadList: loadList,
-        loadDetails: loadDetails,
-        showDetails: showDetails
-    };
 
-})();
+
+return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
+};
+
+}) ();
 
 pokemonRepository.loadList().then(function () {
     pokemonRepository.getAll().forEach(function (pokemon) {
